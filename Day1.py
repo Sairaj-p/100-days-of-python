5a0c0df626fa0b75ebd0996aadb1973c506b3b16
Sairaj-p <sairaj.patil124@nmims.edu.in>
2022-06-01 23:38:40 +0530

    Delete Day1.py

--- Day1.py
+++
@@ -1,6 +0,0 @@
-#band name generator
-city = input("What city you live in?\n")
-pet_name = input("What is pet's name?\n")
-
-band_name = city +" "+ pet_name
-print("Your band name is : " +band_name)
