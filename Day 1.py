ebde8593c44957c1f66c73b85cc1ac8b24e4fdfa
Sairaj-p <sairaj.patil124@nmims.edu.in>
2022-06-01 23:38:33 +0530

    Create Day 1.py

---
+++ Day 1.py
@@ -0,0 +1,6 @@
+#band name generator
+city = input("What city you live in?\n")
+pet_name = input("What is pet's name?\n")
+
+band_name = city +" "+ pet_name
+print("Your band name is : " +band_name)
