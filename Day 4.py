5552b1faa2cfa6b2002a37ef03922d63bae0f204
Sairaj-p <sairaj.patil124@nmims.edu.in>
2022-06-02 16:59:24 +0530

    Create Day 4.py

---
+++ Day 4.py
@@ -0,0 +1,60 @@
+#Rock Paper Scissor game
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+computer_choice= random.randint(0,3)
+print("Welcom to Rock paper scissor Game")
+user_choice = int(input("0-Rock , 1-paper, 2- scissor\n:"))
+
+print("\n\nYour Choice is:")
+if user_choice ==0:
+    print(rock)
+elif user_choice==1:
+    print(paper)
+else:
+    print(scissors)
+print("\n \nComputer choice is :")
+if computer_choice ==0:
+    print(rock)
+elif computer_choice==1:
+    print(paper)
+else:
+    print(scissors)
+
+#Brain of the game
+if(user_choice == computer_choice):
+    print("It is a tie")
+elif(user_choice == computer_choice+1):
+    print("You wins!")
+elif(computer_choice == user_choice+1):
+    print("Computer wins!")
+elif computer_choice==2 and user_choice==0:
+    print("You win!")
+else:
+    print("Computer wins!")
