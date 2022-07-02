08b48b535dbb9e81dfbb1acb3514ab4d2d0768be
Sairaj-p <sairaj.patil124@nmims.edu.in>
2022-07-02 18:14:22 +0530

    Name Update

---
+++ Day_5.py
@@ -0,0 +1,58 @@
+#strong password generater
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def get_password(no_letters,no_numbers,no_symbols):
+    total = no_letters+no_numbers+no_symbols
+    password = ""
+    letter = random.randint(0,len(letters)-1)
+    password += letters[letter]
+    letter_count =1
+    number_count =0
+    symbol_count = 0
+
+    for no in range(1,total):
+        not_selected = True
+        while not_selected:
+            choice= random.randint(0,3)
+            if choice == 0:
+                if(letter_count<no_letters):
+                    password += letters[random.randint(0,len(letters)-1)]
+                    letter_count += 1
+                    not_selected = False
+                else:
+                    not_selected = True
+            elif choice ==1:
+                if(number_count<no_numbers):
+                    password += numbers[random.randint(0,len(numbers)-1)]
+                    number_count += 1
+                    not_selected = False
+                    
+                else:
+                    not_selected = True
+            else:
+                if(symbol_count<no_symbols):
+                    password += symbols[random.randint(0,len(symbols)-1)]
+                    symbol_count += 1
+                    not_selected = False
+                else:
+                    not_selected = True
+    return password
+
+
+def Body():     
+
+    print("Welcome to the PyPassword Generator!")
+    no_letters= int(input("How many letters would you like in your password?\n")) 
+    no_symbols = int(input(f"How many symbols would you like?\n"))
+    no_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+    password =get_password(no_letters,no_numbers,no_symbols)
+    print(f"Your password is {password}")
+
+if __name__ == '__main__':
+    Body()      
