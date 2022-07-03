351022b481f747815befcf4bf2933a96beb4db32
Sairaj-p <sairaj.patil124@nmims.edu.in>
2022-07-03 08:34:02 +0530

    Update Day_30.py

--- Day_30.py
+++ Day_30.py
@@ -1,1 +1,13 @@
-#sdfghjk
+# Try,except,else,finally Statements
+
+try:
+    file = open(r"Text.txt")
+except FileNotFoundError:
+    file =open(r"Text.txt",mode="w")
+    file.write("Hello World!")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("End")
