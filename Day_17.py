f442d3fffb2dfa0eb73769d34d61a1ade41c5dc0
Sairaj-p <sairaj.patil124@nmims.edu.in>
2022-07-10 17:00:49 +0530

    Quiz App GUI and Modifications

--- Day_17.py
+++ Day_17.py
@@ -13,48 +13,56 @@ class Question():
 
 #Quiz Machine
 class Quiz():
-    def __init__(self,Qlist,lenght) -> None:
+    def __init__(self,Qlist) -> None:
         self.Qlist = Qlist
         self.question_no =0
         self.Question_list =[]
         self.track = 0
-        while self.track<lenght:
-            no = randint(0,len(question_bank)-1)
+        while self.track<len(Qlist):
+            no = randint(0,len(Qlist)-1)
             if not no in self.Question_list:
                 self.Question_list.append(no)
                 self.track += 1
+        
 
     def next_question(self):
         self.Q_no = self.Question_list[self.question_no]
-        ans =input(f"\n-{self.Qlist[self.Q_no].text} (true,false)")
+        ans =f"\n-{self.Qlist[self.Q_no].q}"
         self.question_no += 1
         return ans
+        
 
-    def check_answer(self,answer):
-        if answer == self.Qlist[self.Q_no].ans :
+    def check_answer(self,answer:str):
+        if answer == self.Qlist[self.Q_no].ans:
             return True
         else:
             return False
+    def Question_no(self):
+        return self.question_no
 
 
 #Quiz Brain
-question_bank=[]
-for no in range(len(question_data)):
-    new_q = Question(no)
-    question_bank.append(new_q)
-os.system("cls")
-print(logo)
-print("Welcome to the Quiz Show")
-lenght = int(input("Enter the Lenght of the Quiz:"))
-quiz = Quiz(question_bank,lenght)
-cont = True
-Q_Count =0
-while cont and Q_Count<lenght:
-    ans = quiz.next_question()
-    cont = quiz.check_answer(ans)
+def main():
+    question_bank=[]
+    for no in range(len(question_data)):
+        new_q = Question(no)
+        question_bank.append(new_q)
+    os.system("cls")
+    print(logo)
+    print("Welcome to the Quiz Show")
+    lenght = int(input("Enter the Lenght of the Quiz:"))
+    quiz = Quiz(question_bank,lenght)
+    cont = True
+    Q_Count =0
+    while cont and Q_Count<lenght:
+        ans = quiz.next_question()
+        cont = quiz.check_answer(ans)
+        if cont:
+            Q_Count += 1
+        else:
+            print("\n  Worng Answer \n   Game Over  \n")
     if cont:
-        Q_Count += 1
-    else:
-        print("\n  Worng Answer \n   Game Over  \n")
-if cont:
-    print("\nCongratulation you have Won!!!\n")
+        print("\nCongratulation you have Won!!!\n")
+
+if __name__ == '__main__':
+    main()     
---
+++ Day_34.py
@@ -0,0 +1,89 @@
+#Quiz app gui
+
+import requests
+from Day_17 import Quiz
+from tkinter import *
+
+theme_color ="#375362"
+parameters = {
+"amount":10,
+"category":18,
+"type":"boolean"
+}
+response = requests.get(url="https://opentdb.com/api.php",params=parameters)
+response.raise_for_status()
+data = response.json()
+question_data =data['results']
+
+class Questions():
+    def __init__(self,no):
+        self.q = question_data[no]["question"]
+        self.ans = question_data[no]["correct_answer"]
+
+class QuizInterface():
+    def __init__(self,quiz:Quiz):
+        self.Question_no =0
+        self.quiz = quiz
+        self.c_score =0
+        self.window = Tk()
+        self.window.title("Quizler")
+        self.window.config(padx=20,pady=20,bg=theme_color)
+        self.score_l = Label(text=f"Score:{self.c_score}",bg =theme_color,fg="white")
+        self.canvas = Canvas(height=250,width=300)
+        self. Qusetion =self.canvas.create_text(150,125,text="Question",fill=theme_color,
+        font=("Arial",10,"italic"),width=250)
+        correct_b_img =PhotoImage(file=r"E:\New folder\Flash Card\images\right.png")
+        wrong_b_img =PhotoImage(file=r"E:\New folder\Flash Card\images\wrong.png")
+        
+        self.True_button = Button(image=correct_b_img,highlightthickness=0,background=theme_color,command=self.True_ans)
+        self.False_button = Button(image=wrong_b_img,highlightthickness=0,background=theme_color,command=self.False_ans)
+        self.score_l.grid(row=0,column=1)
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=20)
+        self.True_button.grid(row=2,column=0,pady=20,padx=20)
+        self.False_button.grid(row=2,column=1,pady=20,padx=20)
+        self.next_question_inter()
+        self.window.mainloop()
+
+    def next_question_inter(self):
+        if self.Continue_nq():
+            self.Question_no += 1
+            self.canvas.config(bg="white")
+            self.score_l.config(text=f"Score:{self.c_score}")
+            self.new_q = self.quiz.next_question()
+            self.canvas.itemconfig(self.Qusetion,text=self.new_q)
+        else:
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(self.Qusetion,text=f"The Quiz Is Finished\nYour Score :{self.c_score}")
+            self.True_button.config(state="disabled")
+            self.False_button.config(state="disabled")
+
+    def True_ans(self):
+        self.ans = self.quiz.check_answer("True")
+        self.Check_ans(self.ans)
+        
+    def False_ans(self):
+        self.ans = self.quiz.check_answer("False")
+        self.Check_ans(self.ans)
+
+    def Check_ans(self,ans):
+        if ans == True:
+            self.c_score += 1
+            self.canvas.config(bg="green")
+            
+            self.window.after(1000,self.next_question_inter)     
+        else:
+            self.canvas.config(bg ="red")
+            self.window.after(1000,self.next_question_inter)  
+
+    def Continue_nq(self):
+        if self.quiz.Question_no() < 10:
+            return True
+        else:
+            return False    
+            
+        
+ques_bank =[]
+for _ in range(0,10):
+    ques_bank.append(Questions(_))
+quiz = Quiz(ques_bank)
+interface = QuizInterface(quiz)
